6c4145b6eb4e95921999dfd5d3e67183086d10e5
Felipe Marques <feliperfmarques@gmail.com>
2017-05-22 22:41:51 -0300

    Cleaning up unused import

--- android/ZBar.java
+++ android/ZBar.java
@@ -11,7 +11,6 @@ import android.content.Intent;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.Manifest;
-import android.util.Log;
 
 import org.apache.cordova.PluginResult;
 import org.apache.cordova.PermissionHelper;
